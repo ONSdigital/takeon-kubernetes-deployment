e38ec0f4fc0533e4241d60cde4695447a03c94a4
sufyaniqbal <sufyaniqbal@me.com>
2020-01-16 16:51:45 +0000

    a

--- delete-layers.sh
+++ delete-layers.sh
@@ -10,3 +10,5 @@ fi
 kubectl delete deployment,service business-layer -n $namespace
 kubectl delete deployment,service takeon-ui -n $namespace
 kubectl delete deployment,service graphql -n $namespace
+
+kubectl delete namespace $namespace
