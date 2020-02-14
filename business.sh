6e51b7f597b9037dbbcadb78747f511eb71a817a
mattprice-28 <mattprice_28@hotmail.co.uk>
2020-02-14 13:55:30 +0000

    Added command to log in to aws (must have aws cli installed and configured locally)

--- business.sh
+++ business.sh
@@ -3,7 +3,7 @@
 source env_variables
 . kube_functions
 
-
+$(aws ecr get-login --no-include-email --region eu-west-2)
 echo "Building ${BL_image} from ${BL_repo}"
 docker build -t ${BL_image}:${namespace} ${BL_repo}
 docker tag takeon-dev-bl:${namespace} 014669633018.dkr.ecr.eu-west-2.amazonaws.com/takeon-dev-bl:${namespace}
--- ui.sh
+++ ui.sh
@@ -3,7 +3,7 @@
 source env_variables
 . kube_functions
 
-
+$(aws ecr get-login --no-include-email --region eu-west-2)
 # Build Docker Images
 echo "Building ${UI_image} from ${UI_repo}"
 docker build -t ${UI_image}:${namespace} ${UI_repo}
